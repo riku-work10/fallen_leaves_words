b5fbc8e98b05cc8716a14a1c7423b2864db89274
riku-work10 <ukir19990530@gmail.com>
2025-09-15 18:32:14 +0900

    ひな型作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
